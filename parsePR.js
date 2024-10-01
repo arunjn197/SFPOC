ada29e5a6bd9d85f35670659b0e769342c77a563
Arun Jain <50982043+arunjn197@users.noreply.github.com>
2024-10-01 23:47:54 +0530

    Update parsePR.js

--- parsePR.js
+++ parsePR.js
@@ -7,20 +7,27 @@ async function extractTests(){
     let testsFile = __dirname+'/testsToRun.txt';
     await fs.promises.writeFile(testsFile,'all');
 
-    const lines = readline.createInterface({
-        input: fs.createReadStream(__dirname+'/pr_body.txt'),
+    // const lines = readline.createInterface({
+    //     input: fs.createReadStream(__dirname+'/pr_body.txt'),
+    //     crlfDelay: Infinity
+    // });
+
+    // for await (const line of lines) {
+    //     //special delimeter for apex tests
+    //     if(line.includes('Apex::[') && line.includes(']::Apex')){
+
+    //         let tests = line.substring(7,line.length-7);
+    //         await fs.promises.writeFile(testsFile,tests);
+    //         await fs.promises.appendFile(testsFile,'\n');
+    //     }
+    // }
+     const passed_tests = readline.createInterface({
+        input: fs.createReadStream(__dirname+'/passed_test.txt'),
         crlfDelay: Infinity
     });
-
-    for await (const line of lines) {
-        //special delimeter for apex tests
-        if(line.includes('Apex::[') && line.includes(']::Apex')){
-
-            let tests = line.substring(7,line.length-7);
-            await fs.promises.writeFile(testsFile,tests);
-            await fs.promises.appendFile(testsFile,'\n');
-        }
-    }
+    for await (const line of passed_tests){
+            await fs.promises.writeFile(testsFile,line);
+   }
 }
 
 extractTests();
